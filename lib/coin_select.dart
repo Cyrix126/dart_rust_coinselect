ca97540fc7acab7bf612392aa6ef207fb954eb61
Cyrix126 <58007246+Cyrix126@users.noreply.github.com>
2025-06-28 12:12:44 +0200

    feat: export modules as rust-coinselect

--- lib/coin_select.dart
+++ lib/coin_select.dart
@@ -1,3 +1,13 @@
 library;
 
 export 'src/selectcoin.dart';
+
+export 'src/algorithms/lowestlarger.dart';
+export 'src/algorithms/srd.dart';
+export 'src/algorithms/knapsack.dart';
+export 'src/algorithms/fifo.dart';
+export 'src/algorithms/bnb.dart';
+
+export 'src/types.dart';
+
+export 'src/utils.dart';
